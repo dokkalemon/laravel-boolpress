1abfa67bcbe8b6b6681f79ae9f4eea25adbb6cfb
dokkalemon <dottori.roberto@gmail.com>
2022-02-01 17:02:41 +0100

    add comments

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -4,10 +4,12 @@
     <div class="container mt-5">
         <h1 class="text-primary">Post Archive</h1>
 
+        {{-- Se non ci sono post verrà visualizzato un messaggio --}}
         @if ($posts->isEmpty())
         <div class="alert alert-danger">Non ci sono Post da visualizzare! <a href="{{route('admin.posts.create')}}"><strong>Creane Uno</strong></a></div>
         @else
 
+        {{-- Div per la notifica di cancellazione di un post --}}
         @if(session('deleted')) 
             <div class="alert alert-success">
                 <strong>{{session('deleted')}}</strong>
@@ -28,12 +30,18 @@
                 <tr>
                     <td>{{$post->id}}</td>
                     <td>{{$post->title}}</td>
+
+                    {{-- SHOW BUTTON --}}
                     <td>
                         <a href="{{route('admin.posts.show', $post->slug)}}"><button class="btn btn-primary">SHOW</button></a>
                     </td>
+
+                    {{-- EDIT BUTTON --}}
                     <td>
                         <a href="{{route('admin.posts.edit', $post->id)}}"><button class="btn btn-dark">EDIT</button></a>
                     </td>
+
+                    {{-- DELETE BUTTON --}}
                     <td>
                         <form action="{{route('admin.posts.destroy', $post->id)}}" method="POST">
                             @csrf

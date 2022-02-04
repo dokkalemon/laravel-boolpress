7e4bc30ce1c308c748b56919351444a8473c7165
dokkalemon <dottori.roberto@gmail.com>
2022-02-04 16:40:34 +0100

    create migration for pivot

---
+++ database/migrations/2022_02_04_152641_create_post_tag_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+
+            //Post
+            $table->unsignedBigInteger('post_id');
+            $table->foreign('post_id')
+                    ->references('id')
+                    ->on('posts')
+                    ->delete('cascade');
+
+            //Tag  
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')
+                    ->references('id')
+                    ->on('tags')
+                    ->delete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}

cd5e378608f416ca2909e1e7b6fae684f4418250
dokkalemon <dottori.roberto@gmail.com>
2022-02-14 16:32:56 +0100

    send email when you go in dashboard pagex

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Mail\SendNewMail;
+use Illuminate\Support\Facades\Mail;
+
 class HomeController extends Controller
 {
     public function index() {
+        Mail::to('roberto@test.com')->send(new SendNewMail());
+
         return view('admin.home');
     }
 }
---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>WELCOME</h1>
+
+<p>Welcome to my web site........</p>
